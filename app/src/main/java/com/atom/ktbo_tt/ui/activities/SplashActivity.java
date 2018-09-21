6426c8c690fdb335ed8022e9c4b9facb87c7cd16
Unknown <killerinshadow2@gmail.com>
2018-09-21 14:14:12 +0300

    Update project

--- app/src/main/java/com/atom/ktbo_tt/ui/activities/SplashActivity.java
+++ app/src/main/java/com/atom/ktbo_tt/ui/activities/SplashActivity.java
@@ -3,6 +3,7 @@ package com.atom.ktbo_tt.ui.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.atom.ktbo_tt.Preferences;
 import com.atom.ktbo_tt.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,5 +12,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tryToLoad();
+    }
+
+    private void tryToLoad() {
+
+        // Check if TimeTable exists
+        if (Preferences.getTimeTable() == null) {
+
+            // TODO: Get timetable
+            // TODO: Save timetable
+        } else {
+
+        }
     }
 }
