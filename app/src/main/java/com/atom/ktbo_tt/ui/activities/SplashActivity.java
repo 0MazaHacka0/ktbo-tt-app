d0d950aa630ae66b20a40ba5c2a6c60683bac6df
0MazaHacka0 <killerinshadow2@gmail.com>
2018-09-21 12:38:36 +0300

    Add splash screen

---
+++ app/src/main/java/com/atom/ktbo_tt/ui/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.atom.ktbo_tt.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.atom.ktbo_tt.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
